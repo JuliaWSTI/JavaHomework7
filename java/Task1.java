76f7de64d9f7965af6eb0ba193b2e803a5d97e20
JuliaWSTI <juliako0011@gmail.com>
2025-07-03 23:29:49 +0200

    Initial commit

---
+++ java/Task1.java
@@ -0,0 +1,35 @@
+package java;
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class Task1 {
+    public static void main(String[] args) {
+        String nazwahosta = (args.length > 0) ? args[0] : "time-a.nist.gov";                              ;
+
+            try{
+                Socket gniazdo = new Socket(nazwahosta,37);
+                InputStream strumien = gniazdo.getInputStream();
+
+                //Read 4 bytes
+                int b1 = strumien.read();
+                int b2 = strumien.read();
+                int b3 = strumien.read();
+                int b4 = strumien.read();
+
+                // liczba sekund of 1900
+                long sekundyOd1900 = ((long)b1 << 24) | ((long)b2 <<16) | ((long)b3<<8) | ((long)b4);
+
+                //zgodność z JAVA
+                long sekundyOd1970 = sekundyOd1900 - 2208988800L;
+
+                Date time = new Date(sekundyOd1970 * 1000L);
+                System.out.println("Czas z serwera: " + nazwahosta);
+
+                gniazdo.close();
+
+    }catch (IOException e){
+            System.out.println(e);}
+    }
+}
